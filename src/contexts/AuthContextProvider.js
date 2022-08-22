969e6fe543bb98a745b00d5abb05f59ab608971e
seyitakm <seyit200020@gmail.com>
2022-08-22 17:59:33 +0600

    auth

--- src/contexts/AuthContextProvider.js
+++ src/contexts/AuthContextProvider.js
@@ -1,10 +1,28 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const authContext = React.createContext();
 export const useAuth = () => useContext(authContext);
 
-const API = "";
+const API = "https://makers-clinic.herokuapp.com/docs/";
+
 const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState();
+  const [error, setError] = useState();
+
+  const navigate = useNavigate();
+
+  const register = async (formData) => {
+    try {
+      const res = await axios.post(`${API}/account/register/`, formData);
+      console.log(res);
+      navigate("/login");
+    } catch (error) {
+      console.log(Object.values(error.response.data).flat(2)[0]);
+      console.log(error);
+      setError(Object.values(error.response.data).flat(2)[0]);
+    }
+  };
   return <authContext.Provider value={{}}>{children}</authContext.Provider>;
 };
 
