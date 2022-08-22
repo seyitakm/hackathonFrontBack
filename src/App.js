38df1927bf81bc7743c59241159950e1de53f695
Beksultan24 <beksbor05@gmail.com>
2022-08-22 16:50:56 +0300

    navbar-auth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import AuthContextProvider from "./contexts/AuthContextProvider";
 import MainRoutes from "./MainRoutes";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <AuthContextProvider>
+        <Navbar />
         <MainRoutes />
       </AuthContextProvider>
     </>
