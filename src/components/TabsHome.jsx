9b4a2c0c7ccf3fa0659cbe45a7ee65e3c0b3cd6f
kgklyn <azazaklyn@gmail.com>
2022-08-27 10:53:27 +0300

    add tabs

---
+++ src/components/TabsHome.jsx
@@ -0,0 +1,104 @@
+import React from "react";
+import "./Styles/TabsHome.css";
+
+const TabsHome = () => {
+  return (
+    <div className="tabs-box">
+      <article class="tabs">
+        <section id="tab1">
+          <h2>
+            <a href="#tab1">Направления</a>
+          </h2>
+          <span className="tabs-section">
+            <span className="tabs-content">
+              <span className="tabs-text">
+                <a href="#">Аллергология</a>
+                <a href="#">Гастроэнтерология</a>
+                <a href="#">Гинекология</a>
+                <a href="#">Дерматология</a>
+                <a href="#">Кардиология</a>
+                <a href="#">Лечебная физкультура(ЛФК)</a>
+                <a href="#">Маммология</a>
+              </span>
+              <span className="tabs-text">
+                <a href="#">Массаж</a>
+                <a href="#">Неврология</a>
+                <a href="#">Нейрохирургия</a>
+                <a href="#">Отоларингология</a>
+                <a href="#">Офтальмология</a>
+                <a href="#">Педиатр</a>
+                <a href="#">Пластическая хирургия</a>
+              </span>
+              <span className="tabs-text">
+                <a href="#">Проктология</a>
+                <a href="#">Процедурный кабинет</a>
+                <a href="#">Рентген кабинет</a>
+                <a href="#">Рефлексотерапия</a>
+                <a href="#">Соляная комната</a>
+                <a href="#">Стационар</a>
+                <a href="#">Терапия</a>
+              </span>
+              <span className="tabs-text">
+                <a href="#">Травматология</a>
+                <a href="#">УЗИ</a>
+                <a href="#">Урология</a>
+                <a href="#">Физиотерапия</a>
+                <a href="#">Хирургия</a>
+                <a href="#">Стамотология</a>
+                <a href="#">Эндокринология</a>
+              </span>
+            </span>
+          </span>
+        </section>
+
+        <section id="tab2">
+          <h2>
+            <a href="#tab2">Анализы</a>
+          </h2>
+          <span className="tabs-section">
+            <span className="tabs-content">
+              <span className="tabs-text">
+                <a href="#">Гемотологические исследования</a>
+                <a href="#">Общеклинические исследования</a>
+                <a href="#">Печеночные тесты</a>
+                <a href="#">Почечные тесты</a>
+                <a href="#">Показатели углеводного обмена</a>
+                <a href="#">Показатели липидного обмена</a>
+                <a href="#">Ферменты</a>
+              </span>
+              <span className="tabs-text">
+                <a href="#">Неорганические вещества</a>
+                <a href="#">Свертывающая система</a>
+                <a href="#">Иммунологические методы исследования</a>
+                <a href="#">Диагностика сифилиса</a>
+                <a href="#">Ревматесты</a>
+                <a href="#">Диагностика бруцеллеза</a>
+                <a href="#">Маркеры гепатитов</a>
+              </span>
+              <span className="tabs-text">
+                <a href="#">Витамины и др.биологические соединения</a>
+                <a href="#">Урогенитальная и внутриутробная инфекция</a>
+                <a href="#">Гельминты</a>
+                <a href="#">Другие инфекции</a>
+                <a href="#">Горманальные исследования</a>
+                <a href="#">Онкомаркеры</a>
+                <a href="#">Аллергические тесты</a>
+              </span>
+              <span className="tabs-text">
+                <a href="#">ДНК-диагностика инфекции методом ПЦР</a>
+                <a href="#">ДНК-диагностика гепатитов методом ПЦР</a>
+                <a href="#">Микробиология</a>
+                <a href="#">Взятие материала</a>
+                <a href="#">Тесты на антитела</a>
+                <a href="#">Пцр-тесты</a>
+                <a href="#">Рентген</a>
+              </span>
+            </span>
+          </span>
+        </section>
+      </article>
+    </div>
+  );
+};
+
+export default TabsHome;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+
 import Footer from "../components/Footer";
 import SliderHome from "../components/SliderHome";
+import TabsHome from "../components/TabsHome";
 
 const HomePage = () => {
   return (
     <div>
       <SliderHome />
+      <TabsHome />
+
       <Footer />
     </div>
   );
